590202bfb2e1473aab4fb7faa98e2c1cadd61173
Alan Silva <60674021+AlanMSilva@users.noreply.github.com>
2020-11-21 16:06:34 -0600

    Add files via upload

---
+++ actividad_final/actividad final/login/js/nav.js
@@ -0,0 +1,29 @@
+const usernav = document.getElementById('usernav');
+const close = document.getElementById('close');
+
+let bdD = JSON.parse(localStorage.getItem("Deudas"));
+
+let username = JSON.parse(localStorage.getItem('user'));
+let nombresito =username[0].usuario;
+
+console.log(nombresito)
+if(username != null){
+    usernav.innerHTML=' <a href="#" id="close" class="nav-item nav-link active">'+ username[0].usuario +'</a>'
+}else{
+    usernav.innerHTML=' <a href="index.html" id="close" class="nav-item nav-link active">Iniciar sesión</a>'
+}
+close.addEventListener('click',function(){
+    location.href='index.html';
+});
+
+document.getElementById("btnDeudasD").addEventListener("click",()=>{
+    let texto = document.getElementById("detallesD");
+    bdD.forEach(element => {
+        let nombre = element.nombre;
+        if(nombresito == nombre){
+            texto.innerHTML=`<div class="card">
+            <p> Nombre: ${nombre} Deuda: ${element.deudaTotal   }</p>
+            `
+        }
+    });
+});
